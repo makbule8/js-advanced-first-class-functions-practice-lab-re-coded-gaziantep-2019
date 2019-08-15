b66ac6e1d05847e542c45b6703a771c27c704e72
makbule özbay <makbuleozbay8@gmail.com>
2019-08-15 09:27:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  return drivers.forEach(function(driver) {
+  console.log(driver.name);
+ });
+}
+
+
+function logDriversByHometown(drivers, hometown) {
+  return drivers.forEach(function(driver) {
+    if (driver.hometown === hometown)
+      console.log(driver.name);
+    });
+  }
+
+function driversByRevenue(drivers) {
+  return drivers.slice().sort(function(m,b) {
+      return m.revenue - b.revenue;
+    });
+  }
+
+function driversByName(drivers) {
+  return drivers.slice().sort(function(m,b) {
+      return m.name.localeCompare(b.name);
+    });
+  }
+
+  function totalRevenue(drivers) {
+      return drivers.reduce((total, currentDriver) => {
+        return total + currentDriver.revenue;
+      }, 0);
+    }
+
+    function averageRevenue(drivers) {
+        return drivers.reduce((total, currentDriver) => {
+          return total + currentDriver.revenue;
+        }, 0) / drivers.length;}
